bc7613f5dae29e21bd31c098d9652a2163e46c68
Devine Lu Linvega <aliceffekt@gmail.com>
2016-06-14 15:18:30 -0700

    New type of scene

--- Kelp/World.swift
+++ Kelp/World.swift
@@ -10,33 +10,44 @@ import Foundation
 class World : Empty
 {
 	var depths:Array<SCNNode> = []
+	var floor = SCNNode(geometry:SCNPlane(width: 3, height: 3))
 	
 	override init()
 	{
 		super.init()
 		
-		self.position = SCNVector3(0,0,-2)
+		self.position = SCNVector3(0,0,0)
 		
 		var d:Float = 0
 		while d < 5 {
 			let layer = SCNNode(geometry: SCNPlane(width: 3, height: 3))
-			layer.position = SCNVector3(0,0,d)
+			layer.position = SCNVector3(0,0,0)
 			depths.append(layer)
 			addChildNode(layer)
 			d += 1
 		}
+		
+		// Floor
+		floor.geometry?.materials.first?.diffuse.contents = red
+		floor.eulerAngles.x = degToRad(-90)
+		floor.opacity = 0.1
+		addChildNode(floor)
+		
+		
 		enter(collection.mirror)
 	}
 	
 	func enter(vignette:Vignette)
 	{
+		// Floor
+//		floor.geometry?.materials.first!.diffuse.contents = UIImage(named: "\(vignette.name).floor")
+		
+		// Layers
 		depths[0].geometry?.materials.first!.diffuse.contents = UIImage(named: "\(vignette.name).0")
 		depths[1].geometry?.materials.first!.diffuse.contents = UIImage(named: "\(vignette.name).1")
 		depths[2].geometry?.materials.first!.diffuse.contents = UIImage(named: "\(vignette.name).2")
 		depths[3].geometry?.materials.first!.diffuse.contents = UIImage(named: "\(vignette.name).3")
 		depths[4].geometry?.materials.first!.diffuse.contents = UIImage(named: "\(vignette.name).4")
-		
-//		vignette.whenEnter()
 	}
 	
 	required init(coder aDecoder: NSCoder)
